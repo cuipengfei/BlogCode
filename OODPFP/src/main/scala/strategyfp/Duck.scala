c42d43ac9c15414374ceb3c10bd42fb7afa387b5
cuipengfei <cuipengfei2008@gmail.com>
2015-05-27 11:36:24 +0530

    blog done

--- OODPFP/src/main/scala/strategyfp/Duck.scala
+++ OODPFP/src/main/scala/strategyfp/Duck.scala
@@ -2,14 +2,6 @@ package strategyfp
 
 import strategyfp.Duck.{Fly, Quack, _}
 
-abstract class Duck(f: Fly, q: Quack) {
-  def swim() = println("all ducks float")
-
-  def fly() = f()
-
-  def quack() = q()
-}
-
 object Duck {
   type Fly = () => Unit
   val flyWithWings = () => println("fly with wings")
@@ -18,6 +10,14 @@ object Duck {
   type Quack = () => Unit
   val realQuack = () => println("Quack")
   val muteQuack = () => println("<<silence>>")
+}
+
+abstract class Duck(f: Fly, q: Quack) {
+  def swim() = println("all ducks float")
+
+  def fly() = f()
+
+  def quack() = q()
 }
 
 class MallardDuck extends Duck(flyWithWings, realQuack)
