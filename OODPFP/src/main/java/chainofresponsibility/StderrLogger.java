4d4e82d33725fd775cd74cf452a36a8e21158c1c
cuipengfei <cuipengfei2008@gmail.com>
2015-05-30 00:53:08 +0530

    no comments

--- OODPFP/src/main/java/chainofresponsibility/StderrLogger.java
+++ OODPFP/src/main/java/chainofresponsibility/StderrLogger.java
@@ -9,13 +9,3 @@ class StderrLogger extends Logger {
         System.err.println("Sending to stderr: " + msg);
     }
 }
-
-/*
-The output is:
-   Writing to stdout:   Entering function y.
-   Writing to stdout:   Step1 completed.
-   Sending via e-mail:  Step1 completed.
-   Writing to stdout:   An error has occurred.
-   Sending via e-mail:  An error has occurred.
-   Sending to stderr:   An error has occurred.
-*/
