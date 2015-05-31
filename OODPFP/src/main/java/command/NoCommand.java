9e8cba9a1969b97d05adf79587f8359be20eeaaa
cuipengfei <cuipengfei2008@gmail.com>
2015-05-31 23:35:16 +0530

    remove nocommand

--- OODPFP/src/main/java/command/NoCommand.java
+++
@@ -1,6 +0,0 @@
-package command;
-
-public class NoCommand implements Command {
-    public void execute() {
-    }
-}
--- OODPFP/src/main/java/command/runner/RemoteControl.java
+++ OODPFP/src/main/java/command/runner/RemoteControl.java
@@ -1,7 +1,6 @@
 package command.runner;
 
 import command.Command;
-import command.NoCommand;
 
 public class RemoteControl {
     Command[] onCommands;
@@ -10,12 +9,6 @@ public class RemoteControl {
     public RemoteControl() {
         onCommands = new Command[7];
         offCommands = new Command[7];
-
-        Command noCommand = new NoCommand();
-        for (int i = 0; i < 7; i++) {
-            onCommands[i] = noCommand;
-            offCommands[i] = noCommand;
-        }
     }
 
     public void setCommand(int slot, Command onCommand, Command offCommand) {
