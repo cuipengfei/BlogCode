78c3fa2d58a83c76a0b6c89232d7f30517166b09
cuipengfei <cuipengfei2008@gmail.com>
2015-06-03 15:56:21 +0530

    scala implementation

---
+++ OODPFP/src/main/scala/interpreterS/Expr.scala
@@ -0,0 +1,33 @@
+package interpreterS
+
+trait Expr
+
+case class Plus(left: Expr, right: Expr) extends Expr
+
+case class Minus(left: Expr, right: Expr) extends Expr
+
+case class Number(n: Int) extends Expr
+
+case class Var(name: String) extends Expr
+
+object ExprEval {
+  def eval(expr: Expr, context: Map[String, Expr]): Int = {
+    expr match {
+      case Plus(l, r) => eval(l, context) + eval(r, context)
+      case Minus(l, r) => eval(l, context) - eval(r, context)
+      case Number(n) => n
+      case Var(name) => eval(context(name), context)
+    }
+  }
+
+  def main(args: Array[String]) {
+    val context = Map(
+      ("w", Number(5)),
+      ("x", Number(10)),
+      ("z", Number(42)))
+
+    val expr = Plus(Var("w"), new Minus(new Var("x"), new Var("z")))
+
+    println(eval(expr, context))
+  }
+}
