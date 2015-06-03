f0c7cead62c589221ad5513a51185c78ad9c279f
cuipengfei <cuipengfei2008@gmail.com>
2015-06-03 15:38:01 +0530

    format

--- OODPFP/src/main/java/interpreter/InterpreterExample.java
+++ OODPFP/src/main/java/interpreter/InterpreterExample.java
@@ -5,13 +5,14 @@ import java.util.Map;
 
 public class InterpreterExample {
     public static void main(String[] args) {
-        String expression = "w x z - +";
-        Evaluator sentence = new Evaluator(expression);
-        Map<String, Expression> variables = new HashMap<String, Expression>();
+        Map<String, Expression> variables = new HashMap<>();
         variables.put("w", new Number(5));
         variables.put("x", new Number(10));
         variables.put("z", new Number(42));
+
+        Evaluator sentence = new Evaluator("w x z - +");
         int result = sentence.interpret(variables);
+
         System.out.println(result);
     }
 }
