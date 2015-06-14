c3a14f8372a37d11eedb341563a6361cbbd84c45
cuipengfei <cuipengfei2008@gmail.com>
2015-06-15 00:36:17 +0530

    wik

--- OODPFP/src/main/java/visitorJ/car.java
+++ OODPFP/src/main/java/visitorJ/car.java
@@ -4,14 +4,13 @@ class Car implements ICarElement {
     ICarElement[] elements;
 
     public Car() {
-        //create new Array of elements
-        this.elements = new ICarElement[] { new Wheel("front left"),
-                new Wheel("front right"), new Wheel("back left") ,
-                new Wheel("back right"), new Body(), new Engine() };
+        this.elements = new ICarElement[]{new Wheel("front left"),
+                new Wheel("front right"), new Wheel("back left"),
+                new Wheel("back right"), new Body(), new Engine()};
     }
 
     public void accept(ICarElementVisitor visitor) {
-        for(ICarElement elem : elements) {
+        for (ICarElement elem : elements) {
             elem.accept(visitor);
         }
         visitor.visit(this);
