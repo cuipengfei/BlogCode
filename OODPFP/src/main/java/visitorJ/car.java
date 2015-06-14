9a29fc53300307885a874b38b9bfa7b15699e332
cuipengfei <cuipengfei2008@gmail.com>
2015-06-14 22:53:05 +0530

    wiki

---
+++ OODPFP/src/main/java/visitorJ/car.java
@@ -0,0 +1,20 @@
+package visitorJ;
+
+class Car implements ICarElement {
+    ICarElement[] elements;
+
+    public Car() {
+        //create new Array of elements
+        this.elements = new ICarElement[] { new Wheel("front left"),
+                new Wheel("front right"), new Wheel("back left") ,
+                new Wheel("back right"), new Body(), new Engine() };
+    }
+
+    public void accept(ICarElementVisitor visitor) {
+        for(ICarElement elem : elements) {
+            elem.accept(visitor);
+        }
+        visitor.visit(this);
+    }
+}
+
