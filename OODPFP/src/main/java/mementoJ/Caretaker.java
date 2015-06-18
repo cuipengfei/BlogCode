a955d20ba2f187b50cc02432d863e9a0b309b06c
cuipengfei <cuipengfei2008@gmail.com>
2015-06-18 10:14:26 +0530

    memento j

---
+++ OODPFP/src/main/java/mementoJ/Caretaker.java
@@ -0,0 +1,21 @@
+package mementoJ;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Caretaker {
+    public static void main(String[] args) {
+        List<Originator.Memento> savedStates = new ArrayList<Originator.Memento>();
+
+        Originator originator = new Originator();
+        originator.set("State1");
+        originator.set("State2");
+        savedStates.add(originator.saveToMemento());
+        originator.set("State3");
+        // We can request multiple mementos, and choose which one to roll back to.
+        savedStates.add(originator.saveToMemento());
+        originator.set("State4");
+
+        originator.restoreFromMemento(savedStates.get(1));
+    }
+}
