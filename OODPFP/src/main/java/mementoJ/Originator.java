f0dc59dd44ec7e042d94d46ab6a0c434541c8bee
cuipengfei <cuipengfei2008@gmail.com>
2015-06-18 10:16:48 +0530

    format

--- OODPFP/src/main/java/mementoJ/Originator.java
+++ OODPFP/src/main/java/mementoJ/Originator.java
@@ -1,23 +1,23 @@
 package mementoJ;
 
+import static java.lang.System.out;
+
 class Originator {
     private String state;
-    // The class could also contain additional data that is not part of the
-    // state saved in the memento..
 
     public void set(String state) {
-        System.out.println("Originator: Setting state to " + state);
+        out.println("Originator: Setting state to " + state);
         this.state = state;
     }
 
     public Memento saveToMemento() {
-        System.out.println("Originator: Saving to Memento.");
+        out.println("Originator: Saving to Memento.");
         return new Memento(this.state);
     }
 
     public void restoreFromMemento(Memento memento) {
         this.state = memento.getSavedState();
-        System.out.println("Originator: State after restoring from Memento: " + state);
+        out.println("Originator: State after restoring from Memento: " + state);
     }
 
     public static class Memento {
