02b3ae819f7d9ae0d6102f555d99824848dac0d0
cuipengfei <cuipengfei2008@gmail.com>
2015-06-18 10:15:25 +0530

    line breaks

--- OODPFP/src/main/java/mementoJ/Caretaker.java
+++ OODPFP/src/main/java/mementoJ/Caretaker.java
@@ -5,17 +5,17 @@ import java.util.List;
 
 class Caretaker {
     public static void main(String[] args) {
-        List<Originator.Memento> savedStates = new ArrayList<Originator.Memento>();
+        List<Originator.Memento> savedStates = new ArrayList<>();
 
         Originator originator = new Originator();
         originator.set("State1");
         originator.set("State2");
         savedStates.add(originator.saveToMemento());
-        originator.set("State3");
-        // We can request multiple mementos, and choose which one to roll back to.
-        savedStates.add(originator.saveToMemento());
-        originator.set("State4");
 
+        originator.set("State3");
+        savedStates.add(originator.saveToMemento());
+
+        originator.set("State4");
         originator.restoreFromMemento(savedStates.get(1));
     }
 }
