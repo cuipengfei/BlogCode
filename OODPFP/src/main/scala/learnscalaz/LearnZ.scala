2b6da1761036aec40568a476967ccf0fe646feb0
cuipengfei <cuipengfei2008@gmail.com>
2015-06-24 01:28:27 +0530

    introduce monoid

--- OODPFP/src/main/scala/learnscalaz/LearnZ.scala
+++ OODPFP/src/main/scala/learnscalaz/LearnZ.scala
@@ -1,7 +1,13 @@
 package learnscalaz
 
 object LearnZ {
-  def sum(xs: List[Int]): Int = xs.foldLeft(0)(_ + _)
+  def sum(xs: List[Int]): Int = xs.foldLeft(IntMonoid.mzero)(IntMonoid.mappend)
+
+  object IntMonoid {
+    def mappend(a: Int, b: Int): Int = a + b
+
+    def mzero: Int = 0
+  }
 
   def main(args: Array[String]) {
     sum(List(1, 2, 3, 4))
