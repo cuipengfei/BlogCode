b6b69ae4a5d8d0fd3be0a03b1922d1b28d8a8bad
cuipengfei <cuipengfei2008@gmail.com>
2015-06-26 23:19:38 +0530

    scala version

---
+++ OODPFP/src/main/scala/templatemethodS/Beverages.scala
@@ -0,0 +1,36 @@
+package templatemethodS
+
+object Beverages {
+  type Brew = () => Unit
+  type AddCondiments = () => Unit
+  type CaffeineBeverage = () => Unit
+
+  def caffeineBeverageWith(brew: Brew, addCondiments: AddCondiments): CaffeineBeverage =
+    () => {
+      boilWater()
+      brew()
+      pourInCup()
+      addCondiments()
+    }
+
+  val brewCoffee: Brew = () => println("Dripping Coffee through filter")
+  val addMilkSugar: AddCondiments = () => println("Adding Sugar and Milk")
+  val brewTea: Brew = () => println("Steeping the tea")
+  val addLemon: AddCondiments = () => println("Adding Lemon")
+
+  private def boilWater() = {
+    println("Boiling water")
+  }
+
+  private def pourInCup() = {
+    println("Pouring into cup")
+  }
+
+  def main(args: Array[String]) {
+    val coffee: CaffeineBeverage = caffeineBeverageWith(brewCoffee, addMilkSugar)
+    coffee()
+
+    val tea: CaffeineBeverage = caffeineBeverageWith(brewTea, addLemon)
+    tea()
+  }
+}
