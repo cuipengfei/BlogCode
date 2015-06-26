7a8419c5c10b4c412b53809ad032270a1c545625
cuipengfei <cuipengfei2008@gmail.com>
2015-06-26 23:04:36 +0530

    TM pattern j

---
+++ OODPFP/src/main/java/templatemethodJ/BeverageTestDrive.java
@@ -0,0 +1,15 @@
+package templatemethodJ;
+
+public class BeverageTestDrive {
+	public static void main(String[] args) {
+ 
+		Tea tea = new Tea();
+		Coffee coffee = new Coffee();
+ 
+		System.out.println("\nMaking tea...");
+		tea.prepareRecipe();
+ 
+		System.out.println("\nMaking coffee...");
+		coffee.prepareRecipe();
+	}
+}
