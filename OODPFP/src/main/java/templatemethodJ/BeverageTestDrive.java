55e512ac642797908905a0fdffe6c1eb6d8b656b
cuipengfei <cuipengfei2008@gmail.com>
2015-06-26 23:04:52 +0530

    format

--- OODPFP/src/main/java/templatemethodJ/BeverageTestDrive.java
+++ OODPFP/src/main/java/templatemethodJ/BeverageTestDrive.java
@@ -1,15 +1,15 @@
 package templatemethodJ;
 
 public class BeverageTestDrive {
-	public static void main(String[] args) {
- 
-		Tea tea = new Tea();
-		Coffee coffee = new Coffee();
- 
-		System.out.println("\nMaking tea...");
-		tea.prepareRecipe();
- 
-		System.out.println("\nMaking coffee...");
-		coffee.prepareRecipe();
-	}
+    public static void main(String[] args) {
+
+        Tea tea = new Tea();
+        Coffee coffee = new Coffee();
+
+        System.out.println("\nMaking tea...");
+        tea.prepareRecipe();
+
+        System.out.println("\nMaking coffee...");
+        coffee.prepareRecipe();
+    }
 }
--- OODPFP/src/main/java/templatemethodJ/Coffee.java
+++ OODPFP/src/main/java/templatemethodJ/Coffee.java
@@ -1,10 +1,11 @@
 package templatemethodJ;
 
 public class Coffee extends CaffeineBeverage {
-	public void brew() {
-		System.out.println("Dripping Coffee through filter");
-	}
-	public void addCondiments() {
-		System.out.println("Adding Sugar and Milk");
-	}
+    public void brew() {
+        System.out.println("Dripping Coffee through filter");
+    }
+
+    public void addCondiments() {
+        System.out.println("Adding Sugar and Milk");
+    }
 }
